cecdcaa83ef431e635635fedde5d4341d68d1877
Hugo Sanchez Garcia <al222010010@gmail.com>
2026-08-16 22:15:30 -0600

    Cambios en la clase HolaMundo estando en rama desarrollo

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,7 +1,10 @@
 public class HolaMundo {
 
     public static void main(String[] args) {
+
         System.out.println("Hola Mundo");
+        System.out.println("Cambios realizados en la rama desarrollo");
+
     }
 
 }
